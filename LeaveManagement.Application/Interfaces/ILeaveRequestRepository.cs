be865ab2055fe81d88da4d0912ddfee4f69333ad
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-02-18 23:18:25 +0530

    Implement LeaveRequestRepository

---
+++ LeaveManagement.Application/Interfaces/ILeaveRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagement.Domain.Entities;
+using LeaveManagement.Domain.Enums;
+
+namespace LeaveManagement.Application.Interfaces
+{
+    public interface ILeaveRequestRepository
+    {
+        Task<LeaveRequest?> GetByIdAsync(Guid id);
+        Task<IEnumerable<LeaveRequest>> GetAllAsync();
+        Task<IEnumerable<LeaveRequest>> GetByEmployeeIdAsync(int employeeId);
+        Task<IEnumerable<LeaveRequest>> GetByStatusAsync(LeaveStatus status);
+        Task CreateAsync(LeaveRequest leaveRequest);
+        Task UpdateAsync(LeaveRequest leaveRequest);
+        Task DeleteAsync(Guid id);
+    }
+}
