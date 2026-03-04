fe4f1b87d68b81de3c59ab677a6a7fa049a58d10
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-03-04 21:46:49 +0530

    define error response model

---
+++ LeaveManagement.API/Models/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.API.Models
+{
+    public class ApiErrorResponse
+    {
+        public string Title { get; set; } = string.Empty;
+        public int Status { get; set; }
+        public string Detail { get; set; } = string.Empty;
+        public Dictionary<string, string[]>? Errors { get; set; }
+    }
+}
