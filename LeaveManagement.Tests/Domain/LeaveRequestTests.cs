f2b9d5536ddb7f3d811c0315dbc1da2263c8cf79
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-03-17 21:50:24 +0530

    Refactor to remove LeaveRequest duplication

--- LeaveManagement.Tests/Domain/LeaveRequestTests.cs
+++ LeaveManagement.Tests/Domain/LeaveRequestTests.cs
@@ -6,10 +6,12 @@ namespace LeaveManagement.Tests.Domain
 {
     public class LeaveRequestTests
     {
-        [Fact]
-        public void Approve_WhenStatusIsPending_ShouldTransitionToApproved()
+        private readonly LeaveRequest _pendingLeaveRequest;
+        private readonly LeaveRequest _approvedLeaveRequest;
+
+        public LeaveRequestTests()
         {
-            var leaveRequest = new LeaveRequest
+            _pendingLeaveRequest = new LeaveRequest
             {
                 Id = Guid.NewGuid(),
                 SubmittedDate = DateTime.UtcNow,
@@ -20,6 +22,23 @@ namespace LeaveManagement.Tests.Domain
                 LeaveStatus = LeaveStatus.Pending,
             };
 
+            _approvedLeaveRequest = new LeaveRequest
+            {
+                Id = Guid.NewGuid(),
+                SubmittedDate = DateTime.UtcNow,
+                EmployeeId = 2,
+                LeaveType = LeaveType.Annual,
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(5),
+                LeaveStatus = LeaveStatus.Canceled,
+            };
+        }
+
+        [Fact]
+        public void Approve_WhenStatusIsPending_ShouldTransitionToApproved()
+        {
+            var leaveRequest = _pendingLeaveRequest;
+
             leaveRequest.Approve(auditorId: 2, comments: "Looks good");
 
             Assert.Equal(LeaveStatus.Approved, leaveRequest.LeaveStatus);
@@ -28,16 +47,7 @@ namespace LeaveManagement.Tests.Domain
         [Fact]
         public void Approve_WhenStatusIsPending_ShouldCreateAuditRecord()
         {
-            var leaveRequest = new LeaveRequest
-            {
-                Id = Guid.NewGuid(),
-                SubmittedDate = DateTime.UtcNow,
-                EmployeeId = 2,
-                LeaveType = LeaveType.Annual,
-                StartDate = DateTime.UtcNow,
-                EndDate = DateTime.UtcNow.AddDays(5),
-                LeaveStatus = LeaveStatus.Pending,
-            };
+            var leaveRequest = _pendingLeaveRequest;
 
             leaveRequest.Approve(auditorId: 3, comments: "Looks good");
 
@@ -53,16 +63,7 @@ namespace LeaveManagement.Tests.Domain
         [Fact]
         public void Approve_WhenStatusIsNotPending_ShouldThrowInvalidLeaveStatusException()
         {
-            var leaveRequest = new LeaveRequest
-            {
-                Id = Guid.NewGuid(),
-                SubmittedDate = DateTime.UtcNow,
-                EmployeeId = 2,
-                LeaveType = LeaveType.Annual,
-                StartDate = DateTime.UtcNow,
-                EndDate = DateTime.UtcNow.AddDays(5),
-                LeaveStatus = LeaveStatus.Canceled,
-            };
+            var leaveRequest = _approvedLeaveRequest;
 
             Assert.Throws<InvalidLeaveStatusException>(() => leaveRequest.Approve(2, ""));
         }
