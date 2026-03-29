185a2d2c1d3fcdbcc5721e1d8d826f533e7bc858
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-03-29 19:24:46 +0530

    update pagination params

--- LeaveManagement.Application/Common/PaginationParams.cs
+++ LeaveManagement.Application/Common/PaginationParams.cs
@@ -2,7 +2,13 @@
 {
     public class PaginationParams
     {
-        public int PageSize { get; set; }
-        public int Page { get; set; }
+        private const int MaxPageSize = 100;
+        public int Page { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize; 
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
     }
 }
