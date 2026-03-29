6be8d45ffc57c4679424be0f6d8edf79e7e5ce84
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-03-29 22:02:12 +0530

    Make PagedResponse generic

--- LeaveManagement.API/Models/PagedResponse.cs
+++ LeaveManagement.API/Models/PagedResponse.cs
@@ -1,12 +1,11 @@
-﻿using LeaveManagement.Application.DTOs;
-
-namespace LeaveManagement.API.Models
+﻿namespace LeaveManagement.API.Models
 {
-    public class PagedResponse
+    public class PagedResponse<T>
     {
-        public IEnumerable<LeaveRequestSummaryDto> dtos { get; set; }
+        public IEnumerable<T> Items { get; set; }
         public int TotalCount { get; set; }
         public int PageSize { get; set; }
         public int Page { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
 }
