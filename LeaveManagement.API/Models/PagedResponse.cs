4169374d76ac989b83ee90cda24a822219da318a
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-04-05 12:21:10 +0530

    Fix: warning

--- LeaveManagement.API/Models/PagedResponse.cs
+++ LeaveManagement.API/Models/PagedResponse.cs
@@ -2,7 +2,7 @@
 {
     public class PagedResponse<T>
     {
-        public IEnumerable<T> Items { get; set; }
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
         public int TotalCount { get; set; }
         public int PageSize { get; set; }
         public int Page { get; set; }
