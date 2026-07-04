cefc0858adce18bf1b12577e31a0453c1acaf7b7
Anuvind Ashok <anuvind.ashok10@gmail.com>
2026-07-04 11:46:18 +0530

    Add xml comments for documentation

--- LeaveManagement.Application/Interfaces/ICurrentUserService.cs
+++ LeaveManagement.Application/Interfaces/ICurrentUserService.cs
@@ -1,8 +1,21 @@
 ﻿namespace LeaveManagement.Application.Interfaces
 {
+    /// <summary>
+    /// Provides access to the security context of the currently authenticated user.
+    /// </summary>
     public interface ICurrentUserService
     {
+        /// <summary>
+        /// Gets the unique integer ID of the currently authenticated user.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">Thrown if the user ID claim is missing or invalid.</exception>
         public int UserId { get; }
+
+        /// <summary>
+        /// Determines whether the current user belongs to the specified security role.
+        /// </summary>
+        /// <param name="role">The name of the role to check.</param>
+        /// <returns>True if the user is in the role; otherwise, false.</returns>
         bool IsInRole(string role);
     }
 }
